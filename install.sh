5d6ffc072a8dd8df50350df3d267894c0f9dbcda
Marvin Kuurstra <marvin@kuurstra.com>
2021-12-23 12:52:22 +0100

    Reorganize

--- install.sh
+++ install.sh
@@ -21,28 +21,29 @@ else
 fi
 
 # Install links
-ln -fs ${DOTFILES_DIR}/runcom/.zprofile ${HOME}/.zprofile
-ln -fs ${DOTFILES_DIR}/runcom/.zshrc ${HOME}/.zshrc
-ln -fs ${DOTFILES_DIR}/git/.gitconfig ${HOME}/.gitconfig
+ln -fs ${DOTFILES_DIR}/profile/zprofile ${HOME}/.zprofile
+ln -fs ${DOTFILES_DIR}/profile/zshrc ${HOME}/.zshrc
+ln -fs ${DOTFILES_DIR}/apps/git/gitconfig ${HOME}/.gitconfig
+ln -fs ${DOTFILES_DIR}/apps/ssh/config ${HOME}/.ssh/config
 
 # Create initial local config if it does not exist yet
 if [ ! -f ${HOME}/.gitconfig.local ]; then
   cat >~/.gitconfig.local  <<EOL
 [include]
-  path = ~/.dotfiles/git/.gitconfig.prive
+  path = ~/.dotfiles/apps/git/gitconfig.prive
 EOL
 
   if test -f "/etc/wsl.conf"; then
     cat >>~/.gitconfig.local  <<EOL
   # Include config for WSL
-  path = ~/.dotfiles/git/.gitconfig.wsl
+  path = ~/.dotfiles/apps/git/gitconfig.wsl
 EOL
   fi
 
   if is-macos; then
     cat >>~/.gitconfig.local  <<EOL
   # Include config for MacOS
-  path = ~/.dotfiles/git/.gitconfig.macos
+  path = ~/.dotfiles/apps/git/gitconfig.macos
 EOL
   fi
 
