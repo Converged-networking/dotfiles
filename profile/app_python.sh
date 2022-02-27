fa83a3734787b6356f8b2ed91cab5e22f81815a1
Marvin Kuurstra <marvin@kuurstra.com>
2022-02-27 22:35:47 +0100

    Stop using poetry and don't create venv in project dir

--- profile/app_python.sh
+++ profile/app_python.sh
@@ -4,17 +4,3 @@ if type "pyenv" > /dev/null; then
     export PATH="$PYENV_ROOT/bin:$PATH"
     eval "$(pyenv init -)"
 fi
-
-# Poetry settings
-if type "poetry" > /dev/null; then
-    poetry config virtualenvs.in-project true
-elif type "$HOME/.poetry/bin/poetry" > /dev/null; then
-    export PATH="$HOME/.poetry/bin:$PATH"
-    poetry config virtualenvs.in-project true
-fi
-
-# pipenv settings
-if type "pipenv" > /dev/null; then
-    # Create python virtual env in same dir
-    export PIPENV_VENV_IN_PROJECT=1
-fi
